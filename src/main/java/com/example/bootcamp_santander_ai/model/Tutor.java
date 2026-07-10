8d47fcd8a9fe5fa0b27888d33f76847fde36efa5
Rafael Coutinho <rafaelcoutinholima12345@gmail.com>
2026-07-10 14:09:44 -0300

    feat: adiciona entidade Tutor com relacionamento a Endereco

--- src/main/java/com/example/bootcamp_santander_ai/model/Tutor.java
+++ src/main/java/com/example/bootcamp_santander_ai/model/Tutor.java
@@ -1,4 +1,10 @@
 package com.example.bootcamp_santander_ai.model;
-
+import jakarta.persistence.*;
 public class Tutor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
 }
