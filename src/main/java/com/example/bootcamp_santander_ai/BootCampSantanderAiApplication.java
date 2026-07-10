2716b690cf61994191e3b705ad7c1290aaa1ef01
Rafael Coutinho <rafaelcoutinholima12345@gmail.com>
2026-07-09 21:35:27 -0300

    Initial commit

---
+++ src/main/java/com/example/bootcamp_santander_ai/BootCampSantanderAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootcamp_santander_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCampSantanderAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCampSantanderAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bootcamp_santander_ai/BootCampSantanderAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootcamp_santander_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCampSantanderAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
