03aeea2617aaceb194e2e394cac482b20386d2f4
Rafael Coutinho <rafaelcoutinholima12345@gmail.com>
2026-07-10 14:13:07 -0300

    feat: adiciona entidade Tutor os getter e setters

--- src/main/java/com/example/bootcamp_santander_ai/model/Tutor.java
+++ src/main/java/com/example/bootcamp_santander_ai/model/Tutor.java
@@ -7,4 +7,28 @@ public class Tutor {
     private String nome;
     @ManyToOne
     private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 }
