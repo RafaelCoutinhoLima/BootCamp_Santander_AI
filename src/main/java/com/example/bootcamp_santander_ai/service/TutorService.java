fe23b94ce34cfebbab6a93caf10ba4b3c0f369cc
Rafael Coutinho <rafaelcoutinholima12345@gmail.com>
2026-07-13 11:32:41 -0300

    fix:bug tutorservice findById

--- src/main/java/com/example/bootcamp_santander_ai/service/TutorService.java
+++ src/main/java/com/example/bootcamp_santander_ai/service/TutorService.java
@@ -1,13 +1,11 @@
 package com.example.bootcamp_santander_ai.service;
+
 import com.example.bootcamp_santander_ai.model.Tutor;
 
 public interface TutorService {
     Iterable<Tutor> findAll();
-    Tutor findByiD(Long id);
-
     Tutor findById(Long id);
-
     void insert(Tutor tutor);
-    void update(Long id,Tutor tutor);
+    void update(Long id, Tutor tutor);
     void delete(Long id);
 }
